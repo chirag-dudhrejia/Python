6eedcbcdde98b1cb52d5e0257703003de2504cb6
Chir@g159 <dchirag159@gmail.com>
2024-01-03 23:51:47 +0530

    divisiblity by 13 or 19

---
+++ check_divisiblity_by_13_or_19/check_divisiblity_by_13_or_19.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+num = int(input("Enter Number Here : "))
+
+if num%13 == 0 or num%19 == 0:
+    print("Yes!")
+else:
+    print("No!")
+
+
+# In[ ]:
+
+
+
+
