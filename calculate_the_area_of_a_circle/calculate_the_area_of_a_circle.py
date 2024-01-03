f02b9311fe56d98e3ea1f4b84153f80d455985cd
Chir@g159 <dchirag159@gmail.com>
2024-01-03 23:54:51 +0530

    area of circle

---
+++ calculate_the_area_of_a_circle/calculate_the_area_of_a_circle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from math import pi
+radius = float(input("Enter Radius : "))
+
+print("The area of circle with radius " + str(radius) + " is : " + str(pi*radius**2))
+
+
+# In[ ]:
+
+
+
+
