74ca26d0d455311ce3986c0e861f8284eeb08675
Chir@g159 <dchirag159@gmail.com>
2024-01-05 01:40:20 +0530

    multiplication table

---
+++ multiplication_table/multiplication_table.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+number = int(input("Enter number you want multiplation table of : "))
+
+for i in range(1,11):
+    print(number,"x",i,"=",number*i)
+
+
+# In[ ]:
+
+
+
+
