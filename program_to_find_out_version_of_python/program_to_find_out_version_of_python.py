e4d6b7e51bab1b80fedc6de9976d45866d92a05f
Chir@g159 <dchirag159@gmail.com>
2024-01-06 00:43:37 +0530

    python version

---
+++ program_to_find_out_version_of_python/program_to_find_out_version_of_python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+print("Python Version")
+print(sys.version)
+print("\nVersion Info")
+print(sys.version_info)
+
+
+# In[ ]:
+
+
+
+
