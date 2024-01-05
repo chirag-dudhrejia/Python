f97136e1a873e16ec10060dd527dc17fd33b0e4d
Chir@g159 <dchirag159@gmail.com>
2024-01-06 00:43:03 +0530

    prac

---
+++ prints_the_calendar_for_given_month_and_year/prints_the_calendar_for_given_month_and_year.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import calendar
+month = int(input("Enter Month : "))
+year = int(input("Enter year : "))
+print(calendar.month(year,month))
+
+
+# In[ ]:
+
+
+
+
