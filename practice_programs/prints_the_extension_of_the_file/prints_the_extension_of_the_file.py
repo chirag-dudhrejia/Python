dbb432c057f2620b80db061ebc2407baff8b1f2e
Chir@g159 <dchirag159@gmail.com>
2024-01-10 22:54:21 +0530

    prac

---
+++ practice_programs/prints_the_extension_of_the_file/prints_the_extension_of_the_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+filename = input("Enter Name Here")
+f_exten = filename.split(".")
+print("entension of file", filename, "is", f_exten[-1])
+
+
+# In[ ]:
+
+
+
+
