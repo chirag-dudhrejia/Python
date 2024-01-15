84998ca34534e776daa966f779975790303c2b82
Chir@g159 <dchirag159@gmail.com>
2024-01-15 13:55:43 +0530

    practice programs

---
+++ Basic Programs/accepts_an_integer_(n)-and_computes_the_value_of_n+nn+nnn/accepts_an_integer_(n)-and_computes_the_value_of_n+nn+nnn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+n = int(input("Enter integer here : "))
+n1 = int("%s" %n)
+n2 = int("%s%s" %(n,n))
+n3 = int("%s%s%s" %(n,n,n))
+print(n1+n2+n3)
+
+
+# In[ ]:
+
+
+
+
