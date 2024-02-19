b649b5c6d94b75d89246fe0b06622dabbbdba392
Chir@g159 <dchirag159@gmail.com>
2024-02-19 19:51:43 +0530

    practice program

---
+++ Basic Programs/find_vowels_from_string/find_vowels_from_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+str = input("Enter String Here : ")
+
+for i in str:
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+        print(i)
+
+
+# In[ ]:
+
+
+
+
