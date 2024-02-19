c806ee3f001c49c01f035a5c76b98d329b72ec5e
Chir@g159 <dchirag159@gmail.com>
2024-02-19 19:53:06 +0530

    practice programs

---
+++ Basic Programs/list_and_tuple_with_comma_separated_numbers/list_and_tuple_with_comma_separated_numbers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+values = input("Enter Comma Saperates Values")
+list = values.split(",")
+tuple = tuple(list)
+print('List ',list)
+print('Tuple' ,tuple)
+
