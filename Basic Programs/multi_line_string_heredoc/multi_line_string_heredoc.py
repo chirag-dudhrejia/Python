ff9e7c5cc14f4e73cca46cd3564d1bf709cd7bbe
Chir@g159 <dchirag159@gmail.com>
2024-02-24 21:13:02 +0530

    practice program

---
+++ Basic Programs/multi_line_string_heredoc/multi_line_string_heredoc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("""
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+""")
+
+
+# In[ ]:
+
+
+
+
