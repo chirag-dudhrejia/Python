17112e7e57b3ac142c364c1d6678d7c37b6859b1
Chir@g159 <dchirag159@gmail.com>
2024-05-10 21:51:23 +0530

    matrix pattern of hashes

---
+++ Basic Programs/pattern/matrix_of_hash/matrix_of_hash.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+matrix = int(input("Enter Matrix : "))
+
+for i in range(matrix):
+    for j in range(matrix):
+        print("#", end=" ")
+    print()
+
+
+# In[ ]:
+
+
+
+
