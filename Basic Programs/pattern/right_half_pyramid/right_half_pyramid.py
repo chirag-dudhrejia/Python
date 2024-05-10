17c286776d5d4f7a420d680d2feef06d11c69804
Chir@g159 <dchirag159@gmail.com>
2024-05-10 21:51:55 +0530

    pyramid

---
+++ Basic Programs/pattern/right_half_pyramid/right_half_pyramid.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+row = int(input("Enter number of rows here : "))
+
+for i in range(1,row+1):
+    for j in range(i):
+        print("#", end=" ")
+    print()
+
+
+# In[ ]:
+
+
+
+
